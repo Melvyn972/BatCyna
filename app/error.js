d019c6cc7e150e7d294da083e05bbae5a1c6a185
972am <972.amlex.972@gmail.com>
2025-04-25 16:03:01 +0200

    Update 404

--- app/error.js
+++ app/error.js
@@ -3,8 +3,6 @@
 import Link from "next/link";
 import ButtonSupport from "@/components/ButtonSupport";
 
-// A simple error boundary to show a nice error page if something goes wrong (Error Boundary)
-// Users can contanct support, go to the main page or try to reset/refresh to fix the error
 export default function Error({ error, reset }) {
   return (
     <>
